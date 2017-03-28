93e9003278ed240b77b1511a2ec51aa1160d6bb5
Nerby <t2nerb@gmail.com>
2017-03-27 23:09:48 -0600

    Added route to handle post request for search query

--- html/app/views.py
+++ html/app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, request, render_template, url_for, redirect
 from . import app
 
 @app.route('/', methods=['get'])
@@ -10,3 +10,9 @@ def index():
 def about():
     if request.method == 'GET':
         return render_template('about.html')
+
+@app.route('/search', methods=['post'])
+def search():
+    query = request.form['search']
+    print("Text in search box: '", query, "'")
+    return redirect('/')
