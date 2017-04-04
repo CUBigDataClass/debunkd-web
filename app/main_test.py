79bb5a5cfb404c1a5d406c7abf607f65dc446a07
Nerby <t2nerb@gmail.com>
2017-04-03 20:38:53 -0600

    Added unit tests for initialization, index page and about page

--- app/main_test.py
+++ app/main_test.py
@@ -1,9 +1,21 @@
+from app import app
 import unittest
 
 class FlaskrTestCase(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index(self):
+        result = self.app.get('/')
+
+        self.assertEqual(result.status_code, 200)
+
+    def test_about(self):
+        result = self.app.get('/about')
+
+        self.assertEqual(result.status_code, 200)
 
     def tearDown(self):
         pass
