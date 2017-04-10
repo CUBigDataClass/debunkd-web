c7a54485e8cb419dedb62a0744aeb630af72628e
Nerby <t2nerb@gmail.com>
2017-04-09 23:34:47 -0600

    ..

---
+++ app/app/classes.py
@@ -0,0 +1,10 @@
+
+
+class Query():
+    """
+    Object to contain query and query results
+    """
+
+    def __init__ (self, query="", query_return=None):
+        self.query = query
+        self.query_results = query_return
