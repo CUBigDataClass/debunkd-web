2f7133eef2645c9f50ba9f676291f4f89097ad95
Nerby <t2nerb@gmail.com>
2017-04-16 23:50:03 -0600

    Changed map constructor configurations

--- app/static/us_choropleth.js
+++ app/static/us_choropleth.js
@@ -2,43 +2,67 @@
 
 // Create the chart
 Highcharts.mapChart('container', {
-    chart: {
-      map: 'countries/us/us-all'
-    },
+  chart: {
+    map: 'countries/us/us-all',
+    backgroundColor: '#337bb8',
+    style: {
+        color: 'white',
+        font: 'Montserrat'
+    }
+  },
 
-    title: {
-      text: 'Tweet frequency by state'
-    },
+  title: {
+    text: 'Corrupted states',
+    style: {
+        color: 'white',
+        font: 'Montserrat'
+    }
+  },
 
-    mapNavigation: {
-      enabled: true,
-      buttonOptions: {
-          verticalAlign: 'bottom'
+  legend: {
+    layout: 'horizontal',
+    borderWidth: 0,
+    backgroundColor: 'rgba(255,255,255,0.0)',
+    style: {
+        color: 'white',
+        font: 'Montserrat'
+    },
+    floating: true,
+    verticalAlign: 'top',
+    y: 25
+  },
+
+  mapNavigation: {
+    enabled: true,
+    buttonOptions: {
+        verticalAlign: 'bottom'
+    }
+  },
+
+  colorAxis: {
+    min: 0,
+    minColor: '#ffdad9',
+    maxColor: '#f50000',
+  },
+
+  series: [{
+    data: data,
+    name: 'Tweet frequency',
+    states: {
+      hover: {
+        color: '#BADA55'
       }
     },
-
-    colorAxis: {
-      min: 0
-    },
-
-    series: [{
-      data: data,
-        name: 'Tweet frequency',
-        states: {
-          hover: {
-            color: '#BADA55'
-          }
-        },
-        dataLabels: {
-          enabled: false,
-          format: '{point.name}'
-        }
-      }, {
-        name: 'Separators',
-        type: 'mapline',
-        data: Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
-        color: 'black',
-        showInLegend: false,
-        enableMouseTracking: false
-    }]
+    dataLabels: {
+      enabled: false,
+      format: '{point.name}'
+    }
+    }, {
+      name: 'Separators',
+      type: 'mapline',
+      data: Highcharts.geojson(Highcharts.maps['countries/us/us-all'], 'mapline'),
+      color: 'black',
+      showInLegend: false,
+      enableMouseTracking: false
+  }]
 });
