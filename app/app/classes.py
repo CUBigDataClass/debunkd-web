56f8b2299ef8c88eb11360ddd7c3688cc7d29bf6
Nerby <t2nerb@gmail.com>
2017-04-19 12:16:42 -0600

    Minor changes for map rendering

--- app/app/classes.py
+++ app/app/classes.py
@@ -8,3 +8,4 @@ class Query():
     def __init__ (self, query="", query_return=None):
         self.query = query
         self.query_results = query_return
+        self.states = dict.fromkeys([ 'ma', 'wa', 'ca', 'or', 'wi', 'me', 'mi', 'nv', 'nm', 'co', 'wy', 'co', 'wy', 'ks', 'ne', 'ok', 'mo', 'il', 'in', 'vt', 'ar', 'tx', 'ri', 'al', 'ms', 'nc', 'va', 'ia', 'md', 'de', 'pa', 'nj', 'ny', 'id', 'sd', 'ct', 'nh', 'ky', 'oh', 'tn', 'wv', 'dc', 'la', 'fl', 'ga', 'sc', 'mn', 'mt', 'nd', 'az', 'ut', 'hi', 'ak' ])
